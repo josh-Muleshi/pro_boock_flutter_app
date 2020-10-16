1d01cb95d6cce969347ec45cf6e77a047d343a46
josh muleshi <jmuleshi2@gmail.com>
2020-10-16 22:55:09 +0100

    keep modified the SideBar.dart

--- lib/SideBar/SideBar.dart
+++ lib/SideBar/SideBar.dart
@@ -5,9 +5,13 @@ class SideBar extends StatelessWidget {
   final bool isSidebarOpened = false;
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Positioned(
       top: 0,
       bottom: 0,
+      left: isSidebarOpened ? 0 : 0,
+      right: isSidebarOpened ? 0 : screenWidth - 45,
       child: Row(
       children: [
         Expanded(
