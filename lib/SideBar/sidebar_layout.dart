1e51b330aab38e193c30b86d2bd1d6b8afe4cdc1
josh muleshi <jmuleshi2@gmail.com>
2020-10-16 21:58:49 +0100

    add a new dart file call SideBar

---
+++ lib/SideBar/sidebar_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SideBarLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        HomePage(),
+        SideBar()
+      ],
+    );
+  }
+}
