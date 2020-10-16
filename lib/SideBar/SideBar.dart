6db94199280062078a645a883822d320e09a7546
josh muleshi <jmuleshi2@gmail.com>
2020-10-16 22:49:53 +0100

    customer the SideBar.dart

--- lib/SideBar/SideBar.dart
+++ lib/SideBar/SideBar.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class SideBar extends StatelessWidget {
+
+  final bool isSidebarOpened = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFF262AAA),
+    return Positioned(
+      top: 0,
+      bottom: 0,
+      child: Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: Color(0xFF262AAA),
+          ),
+        ),
+        Align(
+          alignment: Alignment(0, -0.9),
+          child: Container(
+            width: 35,
+            height: 110,
+            color: Color(0xFF262AAA),
+          ),
+        ),
+      ],
+      )
     );
   }
 }
