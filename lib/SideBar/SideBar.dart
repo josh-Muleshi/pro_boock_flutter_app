8c0a110a37dc7bb1a1b90ada28405d26840e4c29
josh muleshi <jmuleshi2@gmail.com>
2020-10-17 00:02:04 +0100

    keep modified the SideBar with animation

--- lib/SideBar/SideBar.dart
+++ lib/SideBar/SideBar.dart
@@ -1,34 +1,98 @@
+import 'dart:async';
+
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
-class SideBar extends StatelessWidget {
+class SideBar extends StatefulWidget {
 
-  final bool isSidebarOpened = false;
+  @override
+    _SileBarState createState() => _SileBarState();
+  }
+
+class _SileBarState extends State<SideBar> with SingleTickerProviderStateMixin<SideBar> {
+  AnimationController _animationController;
+  StreamController<bool> isSidebarOpenedStreamController;
+  Stream<bool> isSidebarOpenedStream;
+  StreamSink<bool> isSidebarOpenedSink;
+  //final bool isSidebarOpened = true;
+  final _animationDuration = const Duration(milliseconds: 500);
+
+  @override
+  void initState() {
+    super.initState();
+      _animationController = AnimationController(vsync: this, duration: _animationDuration);
+      isSidebarOpenedStreamController = PublishSubject<bool>();
+      isSidebarOpenedStream = isSidebarOpenedStreamController.stream;
+      isSidebarOpenedSink = isSidebarOpenedStreamController.sink;
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    isSidebarOpenedStreamController.close();
+    isSidebarOpenedSink.close();
+    super.dispose();
+  }
+
+  void onIconPressed() {
+    final animationStatus = _animationController.status;
+    final isAnimationCompleted = animationStatus == AnimationStatus.completed;
+
+    if (isAnimationCompleted) {
+      isSidebarOpenedSink.add(true);
+      _animationController.reverse();
+    }else{
+      isSidebarOpenedSink.add(true);
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
 
-    return Positioned(
-      top: 0,
-      bottom: 0,
-      left: isSidebarOpened ? 0 : 0,
-      right: isSidebarOpened ? 0 : screenWidth - 45,
-      child: Row(
-      children: [
-        Expanded(
-          child: Container(
-            color: Color(0xFF262AAA),
-          ),
-        ),
-        Align(
-          alignment: Alignment(0, -0.9),
-          child: Container(
-            width: 35,
-            height: 110,
-            color: Color(0xFF262AAA),
-          ),
-        ),
-      ],
-      )
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: isSidebarOpenedStream,
+      builder: (context, isSideBarOpenedAsync){
+        return AnimatedPositioned(
+            duration: _animationDuration,
+            top: 0,
+            bottom: 0,
+            left: isSideBarOpenedAsync.data ? 0 : 0,
+            right: isSideBarOpenedAsync.data ? 0 : screenWidth - 45,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Color(0xFF262AAA),
+                  ),
+                ),
+                Align(
+                    alignment: Alignment(0, -0.9),
+                    child: GestureDetector(
+                      onTap: () {
+                        onIconPressed();
+                      },
+                      child: Container(
+                        width: 35,
+                        height: 110,
+                        color: Color(0xFF262AAA),
+                        alignment: Alignment.centerLeft,
+                        child: AnimatedIcon(
+                          progress: _animationController.view,
+                          icon: AnimatedIcons.menu_close,
+                          color: Color(0xFF1885FD),
+                          size: 25,
+                        ),
+                      ),
+                    )
+                ),
+              ],
+            )
+        );
+      },
     );
   }
 }
